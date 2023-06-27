787da3ce604d3581f3069d4259f1ae32c8384257
Brian Chatterton <brian.chatterton@gmail.com>
2023-06-27 18:38:13 +0100

    Pages Test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,43 +1,137 @@
 <script>
-    import { onMount } from 'svelte';
-    import { fade } from 'svelte/transition';
+    import { writable } from 'svelte/store'
 
-    import Logo from './logo.svelte';
-    import { status } from '$lib/stores/status';
+    export const enabled = writable(JSON.parse(localStorage.getItem('user')));
 
-    onMount(async function () {
-		setTimeout(()=>{ 
-            status.update(s=> {
-                s.online=true;
-                return s;
-            })
-        }, 800);
-	});
+    enabled.subscribe((value) => localStorage.user = JSON.stringify(value));
 </script>
 
-
-{#if $status?.online }
-    <header>
-        <nav>
-            <a href="/deeds">Deeds</a>
-            <a href="/tasks">Tasks</a>
-        </nav>
-    </header>
-    <main transition:fade="{{ duration: 3000 }}" >
-
-    <slot />
+<header>
+    <img src="/Alliance_assistance.png" alt="Alliance Assistance" />
+</header>
+<main>
+    {#if $enabled}
+        <div class="card">
+            <div class="card-warning">
+                <h2 class="card-heading">
+                    Warning: Early Release 
+                    <small>This service is currently only available for select customers who have an active policy with
+                        Alliance Insurance.</small>
+                </h2>
+            </div>
+            <form class="card-form">
+                <h2 class="card-heading">Login:</h2>
+                <div class="input">
+                    <input name="carReg" type="text" class="input-field" required  bind:value={$carReg}/>
+                    <label for="carReg" class="input-label">Car Registration</label>
+                </div>
+                <div class="action">
+                    <button class="action-button" on:click={check}>Next</button>
+                </div>
+            </form>
+            <div class="card-info">
+                <p>By signing up you are agreeing to our <a href="/t&c">Terms and Conditions</a></p>
+            </div>
+        </div>
+    {:else}
+        <slot />
+    {/if}
 </main>
-{/if}
-<Logo />
+<footer>
+    <div class="logos">
+        <img src="/alliance-logo.png" alt="Alliance Assistance" />
+        <img src="/Help24seven.svg" alt="Help 24 Seven" />
+        <img src="/new-eNQue.svg" alt="eNQue" />        
+    </div>
+</footer>
 
 <style>
-    header nav {
-        display: flex;
-        justify-content: center;
+    :global(body) {
+        background-color: #3f1113;
+        padding: 0px;
+        margin: 0px;
     }
 
-    header nav a {
-        margin: 5px;
+    main {
+        padding: 80px 0px 40px 0px;
+        max-width: 100%; 
+        margin: 0 auto !important; 
+        float: none !important; 
+        overflow: auto;
     }
+
+    header, footer {
+        width: 100%;
+        position: fixed;
+        left: 0px;
+        right: 0px;
+        display: flex;
+        justify-content: space-around;
+        background-color: #fff;
+        transition: height 2s;
+    }
+
+    header {
+        top: 0px;
+        height: 80px;
+        border-bottom: solid 1px #555;
+        box-shadow: 0px 0px 5px #555;
+        z-index: 10;
+    }
+
+    header img {
+        max-width: 100vw;
+        object-fit: scale-down;
+    }
+
+    footer {
+        bottom: 0px;
+        border-top: solid 1px #555;
+        box-shadow: 0px 0px -5px #555;
+        z-index: 10;
+        height: 40px;
+    }
+
+    div.logos {
+        height: 35px;
+        margin: 3px;
+        max-width: 100vw;
+        display: inline-flex;
+        justify-content: space-evenly;
+        align-items: center;
+    }
+    
+    div.logos img {
+        height: 35px;
+        max-width: 30%;
+        max-height: 35px;
+        object-fit: scale-down;
+    }
+
+    @media (max-height: 400px) {
+        footer, div.logos img {
+            height: 0px;
+            transition: height 1s;
+        }
+
+        main {
+            padding-bottom: 0px;
+            transition: padding 2s;
+        }
+    }
+
+    @media (max-height: 350px) {
+        header {
+            height: 0px;
+            transition: height 2s;
+        }
+
+        main {
+            padding: 0px;
+            transition: padding 2s;
+        }
+    }
+
+
 
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,57 @@
-<h1>Coming soon!</h1>
+<script>
+	import { writable } from 'svelte/store';
+	import Airtable from 'airtable';
 
-<ul>
-    <li>
-        <a href="/privacy-policy">Privacy Policy</a>
-    </li>
+	const base = new Airtable({apiKey: 'pat714QBPWnkOi3r7.04902f7721e126f9b01d6484d7edf39fbd88484bfe1c2778d40e1d07e9a946fc'}).base('appeQues42dYRwHjz');
+	const user = writable([]);
+	const carReg = writable('');
+	
+	const check= ()=>{
+		base('Customers').select({
+			// filterByFormula: `(Car Registration Number)='${carReg}'`,
+			sort: [
+				{field: 'Surname', direction: 'asc'}
+			]
+		}).eachPage(function page(records, fetchNextPage) {
+			records.forEach(function(record) {
+				user.update((a) => a.push(record.get('Membership No')));
+			});
+			// fetchNextPage();
+		}, function done(error) {
+			console.log(error);
+		});
+	}
+</script>
 
-</ul>
+<div class="container">
+	<!-- code here -->
+	<div class="card">
+		<div class="card-warning">
+			<h2 class="card-heading">
+				Warning: Early Release 
+				<small>This service is currently only available for select customers who have an active policy with
+                    Alliance Insurance.</small>
+			</h2>
+		</div>
+		<form class="card-form">
+			<h2 class="card-heading">Login:</h2>
+            <div class="input">
+				<input name="carReg" type="text" class="input-field" required  bind:value={$carReg}/>
+				<label for="carReg" class="input-label">Car Registration</label>
+			</div>
+			<div class="action">
+				<button class="action-button" on:click={check}>Next</button>
+			</div>
+		</form>
+		<div class="card-info">
+			<p>By signing up you are agreeing to our <a href="#">Terms and Conditions</a></p>
+		</div>
+	</div>
+</div>
+
+{ JSON.stringify(user) }
+
+<style>
 
 
+</style>
