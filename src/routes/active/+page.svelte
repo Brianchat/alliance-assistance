4d2106caf96295e7e1e911bdb8078441aa6b09ac
Brian Chatterton <brian.chatterton@gmail.com>
2023-06-27 19:42:27 +0100

    Active

---
+++ src/routes/active/+page.svelte
@@ -0,0 +1,7 @@
+<div class="card">
+
+    <div class="card-info">
+        <h2 class="card-heading">Help is on the way!</h2>
+        <p>Message Received. You will get a call shortly.</p>
+    </div>
+</div>
