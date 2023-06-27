b4df69ebf474b2dad081c6999419f9f2812c5d83
Brian Chatterton <brian.chatterton@gmail.com>
2023-06-27 19:55:11 +0100

    cancel button

--- src/routes/active/+page.svelte
+++ src/routes/active/+page.svelte
@@ -3,5 +3,25 @@
     <div class="card-info">
         <h2 class="card-heading">Help is on the way!</h2>
         <p>Message Received. You will get a call shortly.</p>
+        <a href="/">Cancel Alert</a>
     </div>
 </div>
+
+<style>
+    .card-heading {
+        color:black;
+        text-transform:uppercase;
+        text-align: center;
+    }
+
+    a {
+        font-size: 1.2rem;
+        text-transform:uppercase;
+        display: inline-block;
+        padding: 3px 10px;
+        background-color: #165573;
+        color: #fff;
+        border-radius: 5px;
+        box-shadow: 0px 0px 5px #555;
+    }
+</style>
